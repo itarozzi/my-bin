62e14987effdee07a3f43af54546083defc26dc6
Ivan Tarozzi <itarozzi@gmail.com>
2013-02-12 15:56:34 +0100

    update AndroVM script

--- utility/androvm-start.sh
+++ utility/androvm-start.sh
@@ -7,15 +7,28 @@
 # http://androvm.org
 # -----------------------------------
 
+VM_MOUNT_CMD="udisks --mount /dev/mapper/volumes-VM"
 VBOX_NAME=androVM_vbox86tp_4.1.1_r4-20121119-gapps-houdini-flash
 ANDROVM_PATH=/media/VM/VirtualBox/AndroVM/AndroVMplayer-linux64
 ANDROVM_OPTIONS="1280 720 160"
 
+# Check for VM path, if not found then mount
+if [ ! -d "$ANDROVM_PATH" ]; then
+  `$VM_MOUNT_CMD`
+fi
+
+
+if [ ! -d "$ANDROVM_PATH" ]; then
+  echo "AndroVM startup script error!"
+  echo "AndroVM Virtual Machine not found"
+
+  exit 10
+fi
 
 # Start VirtualBox machine
 VBoxHeadless --startvm $VBOX_NAME &
 
-sleep 5
+sleep 3
 
 
 # Start AndroVM Player
